2920d53a4dd34e780cc1dfc266e0c449fcfe8f05
Jonatan García Barrera <jonatangarciabarrera@gmail.com>
2022-11-16 10:46:53 +0100

    badge in progress

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 "use strict";
+let cantProductos;
 
 function checkIcon(idIcon) {
     let iconSelected = document.getElementById(idIcon);
@@ -15,9 +16,22 @@ function checkIcon(idIcon) {
     if (iconSelected.classList.contains("bi-cart")){
         iconSelected.classList.remove("bi-cart");
         iconSelected.classList.add("bi-cart-fill");
+
+        addProduct();
     } else if (iconSelected.classList.contains("bi-cart-fill")){
         iconSelected.classList.remove("bi-cart-fill");
         iconSelected.classList.add("bi-cart");
     }
 
+}
+
+function addProduct() {
+    let badge = document.getElementById("badge-cart");
+
+    if (cantProductos === undefined) {
+        badge.removeAttribute("hidden");
+        cantProductos = 1;
+    } else{
+
+    }
 }
